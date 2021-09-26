b599e9d6c7f510100c778a2c4a3c1de9dbdf19b0
[joalsanchezor] <[joalsanchezor@ittepic.edu.mx]>
2021-09-25 22:10:09 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u1_practica2_archivosmemoriainternayexterna/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.ladm_u1_practica2_archivosmemoriainternayexterna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
